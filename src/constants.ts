5354d70854916dcbfdb71380e7eb069e79394e91
ruisantiago <ruisantiagomr@gmail.com>
2023-09-26 15:31:12 +0100

    replace NEXT_PUBLIC_CONTRACT env var

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ require("dotenv").config();
 const appName: string = process.env.NEXT_PUBLIC_APP_TITLE || "MINTBASE";
 const network = process.env.NEXT_PUBLIC_NETWORK || "testnet";
 const proxyContractAddress =
-  process.env.NEXT_PUBLIC_CONTRACT || "1.minsta.mintbus.testnet";
+  process.env.NEXT_PUBLIC_PROXY_MINTER_CONTRACT_ADDRESS || "1.minsta.mintbus.testnet";
 const tokenContractAddress =
   process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS || "minsta.mintspace2.testnet";
 
