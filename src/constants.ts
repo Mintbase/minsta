14aa79bd65ae2e1619dacf2aeab30750626b3339
Ruben Marcus <ruben@rubenmarcus.dev>
2023-10-06 13:53:57 +0100

    fix leaderboard

--- src/constants.ts
+++ src/constants.ts
@@ -4,9 +4,9 @@ const appName: string = process.env.NEXT_PUBLIC_APP_TITLE || "MINTBASE";
 const network = process.env.NEXT_PUBLIC_NETWORK || "testnet";
 const proxyContractAddress =
   process.env.NEXT_PUBLIC_PROXY_MINTER_CONTRACT_ADDRESS ||
-  "1.minsta.mintbus.near";
+  "1.minsta.mintbus.testnet";
 const tokenContractAddress =
-  process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS || "creative.mintbase1.near";
+  process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS || "minsta.mintspace2.testnet";
 
 const mintbaseBaseUrl =
   process.env.NEXT_PUBLIC_BASE_URL || "https://testnet.mintbase.xyz";
