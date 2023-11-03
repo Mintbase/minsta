d1e0ffb7d5389a8b164a76c182e267cfebe3d8d9
Ruben Marcus <ruben@rubenmarcus.dev>
2023-11-03 17:43:13 +0000

    fixes

--- src/app/meta/[slug]/page.tsx
+++ src/app/meta/[slug]/page.tsx
@@ -4,6 +4,7 @@ import {
 } from "@/data/graphqlService";
 import { FETCH_META } from "@/data/queries/meta.graphql";
 import Head from "next/head";
+import { redirect } from 'next/navigation';
 
 
 export async function generateMetadata({
@@ -37,13 +38,16 @@ export default async function Page({ params }: { params: { slug: string } }) {
     network: "mainnet",
   });
 
+  if(!posts?.data?.nft_metadata?.[0]?.media) {
+    redirect('/')
+  }
 
   return (
     <main>
       <Head>
         <title>My page title</title>
       </Head>
-      <MetaPage meta={posts} slug={params.slug} />
+      {posts?.data?.nft_metadata?.[0]?.media && <MetaPage meta={posts} slug={params.slug} /> } 
     </main>
   );
 }
