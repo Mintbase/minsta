37c7b8cbc35dabd5c11c38978ba6fccbfc6cc7d4
ruisantiago <ruisantiagomr@gmail.com>
2024-02-23 12:04:20 +0000

    change gradient + button

--- src/components/Mint.tsx
+++ src/components/Mint.tsx
@@ -32,7 +32,7 @@ export function Mint({
               Try again
             </button>
             <button
-              className="gradientButton w-full text-primaryBtnText rounded px-4 py-2"
+              className="gradientButton w-full text-white rounded px-4 py-2"
               onClick={() => mintImage(currentPhoto)}
             >
               Upload
